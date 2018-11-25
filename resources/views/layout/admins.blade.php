f34f3f0568e07ef59cce6da587c28f96b8697900
ChuZeFang <1346914905@qq.com>
2018-11-25 21:51:11 +0800

    2018-11-25 21:51 xiangmu

--- resources/views/layout/admins.blade.php
+++ resources/views/layout/admins.blade.php
@@ -172,19 +172,28 @@
                                 <i class='icon-angle-down angle-down'>
                                 </i>
                             </a>
-                            <ul class='nav nav-stacked'>
-                                <li>
-                                    <a class='dropdown-collapse' href='#'>
-                                        <i class='icon-caret-right'>
-                                        </i>
-                                        <span>
-                                            添加用户
-                                        </span>
-                                        <i class='icon-angle-down angle-down'>
-                                        </i>
-                                    </a>
-                                </li>
-                            </ul>
+                            <li>
+                                <a class='dropdown-collapse ' href='#'>
+                                    <i class='icon-home'></i>
+                                    <span>用户管理</span>
+                                    <i class='icon-angle-down angle-down'></i>
+                                </a>
+                                <ul class='nav nav-stacked'>
+                                    <li class=''>
+                                        <a href='/admin/user/create'>
+                                            <i class='icon-money'></i>
+                                            <span>添加用户</span>
+                                        </a>
+                                    </li>
+                                     <li class=''>
+                                        <a href='/admin/user'>
+                                            <i class='icon-money'></i>
+                                            <span>管理用户</span>
+                                        </a>
+                                    </li>
+
+                                </ul>
+                            </li>
                         </li>
                         <li>
                             <a class='dropdown-collapse ' href='#'>
